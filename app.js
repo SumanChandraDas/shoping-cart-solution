07a3073568f9b549c7db1b2b4fdbbbdb5110d613
sumanchandradas <suman.chandra.das@gmail.com>
2022-02-16 22:18:40 +0600

    case increase dicrease event

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-document.getElementById('case-plus').addEventListener("click", function () {
-    const caseInput = document.getElementById("case-number");
+
+function updateCaseNumber(isincrement) {
+    const caseInput = document.getElementById('case-number');
     const caseInputValue = parseInt(caseInput.value);
-    caseInput.value = caseInputValue + 1;
-    console.log("caseInput.value");
+
+    if (isincrement == true) {
+        caseInput.value = caseInputValue + 1;
+
+    }
+    else if (caseInput.value > 0 && isincrement == false) {
+        caseInput.value = caseInputValue - 1;
+    }
+    const costTotal = document.getElementById('cost-total');
+    costTotal.innerText = caseInput.value * 58;
+}
+document.getElementById('case-plus').addEventListener("click", function () {
+    updateCaseNumber(true);
 })
+
+document.getElementById('case-minus').addEventListener("click", function () {
+    updateCaseNumber(false);
+});
